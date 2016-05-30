553ac25b31f9a984388801ab30d05aa6c6125af2
dragon <rongjihuang@gmail.com>
2016-05-30 08:59:29 +0800

    koa中间件级联测试

--- koa.js
+++ koa.js
@@ -5,31 +5,35 @@ const port = 3000;
 
 // x-response-time
 app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  this.set('X-Response-Time', ms + 'ms');
+	console.log("m1 pre - %s %s", this.method, this.url);
+	var start = new Date;
+	yield next;
+	var ms = new Date - start;
+	this.set('X-Response-Time', ms + 'ms');
+	console.log("m1 post - %sms\n", ms);
 });
 
 // logger
 app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  var req = this.req;
-  // x-forwarded-for: client, proxy1, proxy2, proxy3
-  var clientIp = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-  console.log("%s %s %s %sms - %s", new Date().toLocaleString(), clientIp, this.method, this.get('X-Response-Time'), this.url);
+	console.log("m2 pre");
+	var start = new Date;
+	yield next;
+	var ms = new Date - start;
+	var req = this.req;
+	// x-forwarded-for: client, proxy1, proxy2, proxy3
+	var clientIp = req.headers['x-forwarded-for'] || 
+		 req.connection.remoteAddress || 
+		 req.socket.remoteAddress ||
+		 req.connection.socket.remoteAddress;
+	console.log("m2 post - %sms %s", ms, clientIp);
 });
 
 // response
 app.use(function *(){
-  this.body = 'Hello World';
+	this.body = 'Hello World';
+	console.log("main");
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
